4a78138281df1dde822c46fd9d656cb847cffa8f
Лысенко Евгений <evgeen@yandex.ru>
2024-03-29 15:19:07 +0300

    Disable vim wrappers. Add vim executable dir to PATH

--- elev.sh
+++ elev.sh
@@ -6,12 +6,12 @@ shift
 args="$@"
 if [ -f "${cygwin}/$command" ]
 then
-  echo nircmdc elevate ${cygwin}/$command $args
+  cmd.exe /c nircmdc elevate ${cygwin}/$command $args
 else
-  echo nircmdc elevate $command $args
+  cmd.exe /c nircmdc elevate $command $args
 fi
 # cmd /c echo %PATH% | tr ';' '\n' |
   # while read -r dir
   # do
     # echo $dir
-  # done 
+  # done
--- gvim.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set vimExe=gvim
-REM echo %0 | grep -q gvim || set vimExe=vim
-for /f "delims=" %%i in ('echo %* ^| sed ^'s,~,c:/cygwin64/home/Evgen,^'') do (
-  start "" "C:\Users\Evgen\Documents\Vim\vim90\%vimExe%.exe" "%%i"
-)
