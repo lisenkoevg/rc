637b8aa38d652bf79fbdfd6eeb5e58ee9dcad400
Лысенко Евгений <evgeen@yandex.ru>
2025-03-08 20:40:26 +0300

    Fix return code for cron.sh

--- cron.sh
+++ cron.sh
@@ -8,10 +8,10 @@ date >> $log
 ts=$(date +%Y%m%d_%H%M%S)
 
 {
-#TypeAndRun History.ini
+# TypeAndRun History.ini
 src=d:/Soft/TypeAndRun/History.ini
 dest=$backupDir/TypeAndRun/History_${ts}.ini
 cp $src $dest
-[[ $(stat -c%s $dest) -lt 10000 ]] && ( echo ERROR: file has too small size && stat $dest )
+[[ $(stat -c%s $dest) -lt 10000 ]] && ( echo ERROR: file has too small size && stat $dest ) || true
 
 } >> $log 2>&1
