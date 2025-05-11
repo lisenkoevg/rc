341db981b677bb49fc57ef59ed711a40c9163d4d
Лысенко Евгений <evgeen@yandex.ru>
2025-05-11 13:50:31 +0300

    Add shared forlder for VmWare Ubuntu64

--- clang-format.bat
+++ clang-format.bat
@@ -1,1 +1,1 @@
-clang-format.exe -style="{BasedOnStyle: llvm, IndentWidth: 2, SortIncludes: false, ColumnLimit: 120}"
+@clang-format.exe -style="{BasedOnStyle: llvm, IndentWidth: 2, SortIncludes: false, ColumnLimit: 120, AlwaysBreakTemplateDeclarations: Yes}"
